0c0afadc669d038922734789d0ec20f19e2d6f35
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-05-08 17:42:26 -0600

    added more structure to project page

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import eachProject from '';
 
 function Project() {
   const projects = [
     {
       title: "Gastronome",
-      technologies: ["JS/Node.js", "Handlebars.js", "Express.js", "PostgreSQL", ""]
+      technologies: ["JS/Node.js", "Handlebars.js", "Express.js", "PostgreSQL", ""],
+      features: [],
+      
     },
     {
 
@@ -24,9 +27,11 @@ function Project() {
   ];
 
   return (
-    <div className="container">
+    <div className="projects-container">
       {projects.map((project) => (
-        <></>
+        <div className="container">
+          <Project title={project.title} technologies={project.technologies} features={}/>
+        </div>
       ))}
     </div>
   )
