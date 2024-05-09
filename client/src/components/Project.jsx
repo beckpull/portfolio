474a602250070597e8916790b6d6ad0916e8a3cb
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-05-08 19:32:32 -0600

    added more projects structure;

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 function eachProject(props) {
+    const { title, description, technologies, features, image } = props;
     return (
         <div className="container">
-            
+        
         </div>
     )
 }
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,36 +1,75 @@
 import React, { Component } from 'react';
-import eachProject from '';
+import eachProject from '../components/Project';
 
 function Project() {
   const projects = [
     {
       title: "Gastronome",
+      description: "",
       technologies: ["JS/Node.js", "Handlebars.js", "Express.js", "PostgreSQL", ""],
       features: [],
-      
+      image: "",
+      video: "",
+      github: "",
+      deployed: ""
     },
     {
-
+      title: "",
+      description: "",
+      technologies: ["", "", "", "", ""],
+      features: [],
+      image: "",
+      video: "",
+      github: "",
+      deployed: ""
     },
     {
-
+      title: "",
+      description: "",
+      technologies: ["", "", "", "", ""],
+      features: [],
+      image: "",
+      video: "",
+      github: "",
+      deployed: ""
     },
     {
-
+      title: "",
+      description: "",
+      technologies: ["", "", "", "", ""],
+      features: [],
+      image: "",
+      video: "",
+      github: "",
+      deployed: ""
     },
     {
-
+      title: "",
+      description: "",
+      technologies: ["", "", "", "", ""],
+      features: [],
+      image: "",
+      video: "",
+      github: "",
+      deployed: ""
     },
     {
-
-    }
+      title: "",
+      description: "",
+      technologies: ["", "", "", "", ""],
+      features: [],
+      image: "",
+      video: "",
+      github: "",
+      deployed: ""
+    },
   ];
 
   return (
     <div className="projects-container">
       {projects.map((project) => (
         <div className="container">
-          <Project title={project.title} technologies={project.technologies} features={}/>
+          <eachProject title={project.title} description={project.description} technologies={project.technologies} features={project.features} image={project.image} video={project.video} github={project.github} deployed={project.deployed}/>
         </div>
       ))}
     </div>
