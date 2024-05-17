dad3fa5ae2ce91ceecb405158895c4be22d7cb2c
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-05-16 19:06:18 -0600

    added blurb on techblog page due to incompleteness

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -3,7 +3,11 @@ import axios from 'axios';
 
 const BlogPosts = () => {
 
-  return 
+  return (
+    <div className='container has-text-centered'>
+      <h4 className='title is-4'>This page is a work in progress and will be updated soon!</h4>
+    </div>
+  ) 
 };
 
 export default BlogPosts;
