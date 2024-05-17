3a12ed2e3b130cdabb6481b0dfb174a2d05c3709
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-05-16 19:22:56 -0600

    edited about me section for more clarity

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
     <div className="my-6">
       <p className="is-size-5">Thank you so much for stopping by! My name is <strong><i>Beck</i></strong>, and I look forward to the possibility of collaborating with you on exciting projects in the future!</p>
       <br />
-      <p className="is-size-5">Born and raised on the east coast, I've cultivated a spirit of resilience and adaptability from an early age. From publishing books as a child to spending two years living in a makeshift tent, I've embraced every experience as an opportunity for growth and learning. With a proven track record of success and a dedication to continuous improvement, I am eager to embark on the next chapter of my professional journey.</p>
+      <p className="is-size-5">Born and raised on the east coast, I've cultivated a spirit of resilience and adaptability from an early age. From helping my grandmother edit and publish books as a child to spending two years living in a makeshift tent, I've embraced every experience as an opportunity for growth and learning. With a proven track record of success and a dedication to continuous improvement, I am eager to embark on the next chapter of my professional journey.</p>
       <br />
       <p className="is-size-5">With a rich tapestry of experiences ranging from aviation management to software engineering, I bring a versatile skill set and a passion for excellence to every endeavor.</p>
       <br />
